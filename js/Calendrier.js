fbf94ef8c4f2d0afa9517b90cfe3f325d24f2b6c
M S <mxsou.ad@gmail.com>
2026-03-24 12:25:17 +0100

    changemnt noms mois

--- js/Calendrier.js
+++ js/Calendrier.js
@@ -8,41 +8,34 @@ let currentMonth = currentDate.getMonth();
 let currentYear = currentDate.getFullYear();
 
 const months = [
-  'January', 'February', 'March', 'April', 'May', 'June',
-  'July', 'August', 'September', 'October', 'November', 'December'
+  'Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin',
+  'Juilet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre'
 ];
 
 function renderCalendar(month, year) {
   calendarDates.innerHTML = '';
   monthYear.textContent = `${months[month]} ${year}`;
 
-  // Get the first day of the month
   const firstDay = new Date(year, month, 1).getDay();
 
-  // Get the number of days in the month
   const daysInMonth = new Date(year, month + 1, 0).getDate();
 
-  // Create blanks for days of the week before the first day
   for (let i = 0; i < firstDay; i++) {
     const blank = document.createElement('div');
     calendarDates.appendChild(blank);
   }
 
-  // Populate the days
   for (let i = 1; i <= daysInMonth; i++) {
     const day = document.createElement('div');
     day.textContent = i;
     calendarDates.appendChild(day);
   }
-  // Get today's date
   const today = new Date();
 
-  // Populate the days
   for (let i = 1; i <= daysInMonth; i++) {
     const day = document.createElement('div');
     day.textContent = i;
 
-    // Highlight today's date
     if (
       i === today.getDate() &&
       year === today.getFullYear() &&
