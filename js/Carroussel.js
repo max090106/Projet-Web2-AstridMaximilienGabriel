dd69d6246a30e27850d900608033fcd15c552395
M S <mxsou.ad@gmail.com>
2026-03-31 16:28:06 +0200

    fin des catousels JS

--- js/Carroussel.js
+++ js/Carroussel.js
@@ -1,24 +1,26 @@
-const track = document.querySelector('.track');
-const slides = document.querySelectorAll('.slide-resp');
-const prevBtn = document.getElementById('prev');
-const nextBtn = document.getElementById('btn-nexts');
+document.querySelectorAll('.carousel-hid').forEach((carousel) => {
+  const track = carousel.querySelector('.track');
+  const slides = carousel.querySelectorAll('.slide-resp');
+  const prevBtn = carousel.parentElement.querySelector('.btn-prev');
+  const nextBtn = carousel.parentElement.querySelector('.btn-next');
 
-let current = 0;
-const total = slides.length;
-const visible = 2;
+  let current = 0;
+  const total = slides.length;
+  const visible = 2;
 
-function goTo(index) {
-  const remainingImg = total - visible;
-  if (index < 0) index = remainingImg;
-  if (index > remainingImg) index = 0;
-  current = index;
-  track.style.transform = `translateX(-${current * (100 / visible)}%)`;
-}
+  function goTo(index) {
+    const remainingImg = total - visible;
+    if (index < 0) index = remainingImg;
+    if (index > remainingImg) index = 0;
+    current = index;
+    track.style.transform = `translateX(-${current * (100 / visible)}%)`;
+  }
 
-prevBtn.addEventListener('click', () => {
-  goTo(current !== 0 ? current - 1 : current);
-});
+  prevBtn.addEventListener('click', () => {
+    goTo(current !== 0 ? current - 1 : current);
+  });
 
-nextBtn.addEventListener('click', () => {
-   goTo(current === 3 ? current - 3 : current + 1);
+  nextBtn.addEventListener('click', () => {
+    goTo(current === total - visible ? current - (total - visible) : current + 1);
+  });
 });
