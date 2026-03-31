46a4bfb4517df0ec6c55af4644d2bab50818f0a8
Gabriel Lallier <gabriel.lallier@efrei.net>
2026-03-31 16:33:46 +0200

    ajout debut php

---
+++ php/header.php
@@ -0,0 +1,14 @@
+<header>
+    <h1>
+        <img src="../img/logo-efrei.png" id="logo" width="430" height="120">
+        DEPARTEMENT INFORMATIQUE DE L'EFREI
+    </h1>
+    
+    <nav>
+        <a href="Accueil.html">Accueil</a>
+        <a href="Formations.html">Formations</a>
+        <a href="Temoignages.html">Témoignages</a>
+        <a href="Apropos.html">A propos</a>
+        <a href="Equipes.html">Equipes</a>
+    </nav>
+</header>
